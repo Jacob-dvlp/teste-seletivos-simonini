7645f79bfcf8c1a8be814ba4fa03b82bee257a05
Jacob Larte <robsonpereirawill@gmail.com>
2022-12-29 11:19:47 +0100

    refactoring getPrecision

--- dist/2-get-precision/get-precision.js
+++ dist/2-get-precision/get-precision.js
@@ -3,13 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPrecision = void 0;
 var getPrecision = function (n) {
     n = Math.abs(n); //Convert negative to positive number
-    var numberPartsByPoint = n.toString().split('.'); //Split number by point
-    var numberPartsByHifen = n.toString().split('e-'); //Split number by hifen
-    if (numberPartsByPoint[1])
+    var valueReturn = 0; //return;
+    var numberPartsByPoint = n.toString().split('.');
+    if (numberPartsByPoint[1]) {
         return numberPartsByPoint[1].length;
-    if (numberPartsByHifen[1])
+    }
+    ;
+    var numberPartsByHifen = n.toString().split('e-');
+    if (numberPartsByHifen[1]) {
         return Number(numberPartsByHifen[1]);
-    return 0;
+    }
+    ;
+    return valueReturn;
 };
 exports.getPrecision = getPrecision;
 //# sourceMappingURL=get-precision.js.map
--- src/2-get-precision/get-precision.ts
+++ src/2-get-precision/get-precision.ts
@@ -1,11 +1,16 @@
 export const getPrecision = (n: number): number => {
   n = Math.abs(n); //Convert negative to positive number
+  const valueReturn = 0 //return;
 
-  const numberPartsByPoint = n.toString().split('.'); //Split number by point
-  const numberPartsByHifen = n.toString().split('e-'); //Split number by hifen
+  const numberPartsByPoint = n.toString().split('.');
+  if (numberPartsByPoint[1]){
+    return numberPartsByPoint[1].length
+  };
+  
+  const numberPartsByHifen = n.toString().split('e-'); 
+  if (numberPartsByHifen[1]) {
+    return Number(numberPartsByHifen[1])
+  };
 
-  if (numberPartsByPoint[1]) return numberPartsByPoint[1].length;
-  if (numberPartsByHifen[1]) return Number(numberPartsByHifen[1]);
-
-  return 0;
+  return valueReturn;
 };
